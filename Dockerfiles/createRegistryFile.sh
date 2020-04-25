f64ab6d605b6fe1ded15a6f3c87a94a9928a06d6
shayaantx <shayaantx@gmail.com>
2020-04-25 19:56:18 -0400

    Fix registry/config file variable replacements

--- Dockerfiles/createRegistryFile.sh
+++ Dockerfiles/createRegistryFile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cp h2server.reg.template h2server.reg
-sed -e "s/\${1}/$1/" -e "s/\${2}/$2/" h2server.reg
+cp h2server.reg.template
+sed -e "s/\${1}/$1/" -e "s/\${2}/$2/" h2server.reg.template | tee h2server.reg
--- Dockerfiles/createServerConfig.sh
+++ Dockerfiles/createServerConfig.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-cp h2serverconfig.ini.template h2serverconfig.ini
-sed -e "s/\${1}/$1/" -e "s/\${2}/$2/" -e "s/\${3}/$3/" -e "s/\${4}/$4/" -e "s/\${5}/$5/" -e "s/\${6}/$6/" -e "s/\${7}/$7/" -e "s/\${8}/$8/" -e "s/\${9}/$9/" -e "s/\${10}/$10/" -e "s/\${11}/$11/" h2serverconfig.ini
+# arguments for this shell script over 9 use ${N} syntax
+sed -e "s/\${var1}/$1/" -e "s/\${var2}/$2/" -e "s/\${var3}/$3/" -e "s/\${var4}/$4/" -e "s/\${var5}/$5/" -e "s/\${var6}/$6/" -e "s/\${var7}/$7/" -e "s/\${var8}/$8/" -e "s/\${var9}/$9/" -e "s/\${var10}/${10}/" -e "s/\${var11}/${11}/" h2serverconfig.ini.template | tee h2serverconfig.ini
