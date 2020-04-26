0c1e5b258bda51e4ee96fdf9685397d44716af31
shayaantx <shayaantx@gmail.com>
2020-04-25 20:25:57 -0400

    Move startup of container to script

---
+++ Dockerfiles/startup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Set the approriate registry settings
+wine regedit h2server.reg
+
+# Verify registry was set
+wine regedit /E output.reg "HKEY_CURRENT_USER\Software\Microsoft\Halo 2\Server\LIVE"
+cat output.reg
+
+wine /home/h2server/h2server.exe -live -h2config=/home/config/h2serverconfig.ini
