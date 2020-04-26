ecf85b89e7b3e5ee842ae14f41edfba9c116c871
shayaantx <shayaantx@gmail.com>
2020-04-25 23:09:45 -0400

    Update startup.sh

--- Dockerfiles/startup.sh
+++ Dockerfiles/startup.sh
@@ -7,4 +7,6 @@ wine regedit h2server.reg
 wine regedit /E output.reg "HKEY_CURRENT_USER\Software\Microsoft\Halo 2\Server\LIVE"
 cat output.reg
 
-wine /home/h2server/h2server.exe -live -h2config=/home/config/h2serverconfig.ini
+# Install the service
+wine /home/h2server/h2server.exe -createservice -live -instance:1 || true
+wine /home/h2server/h2server.exe -service -live -h2config=/home/config/h2serverconfig.ini
