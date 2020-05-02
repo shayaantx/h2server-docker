e0fc08b1dc6e05f8a4d1820a8c43e5c2df1e1ef3
shayaantx <shayaantx@gmail.com>
2020-05-02 17:50:08 -0400

    Fix playlists directory

--- Dockerfiles/startup.sh
+++ Dockerfiles/startup.sh
@@ -5,7 +5,9 @@ wine regedit h2server.reg
 
 # Verify registry was set
 wine regedit /E output.reg "HKEY_CURRENT_USER\Software\Microsoft\Halo 2\Server\LIVE"
-cat output.reg
+
+# Uncomment to troubleshoot whats written to registry
+#cat output.reg
 
 # h2server has an interactive shell that doesn't work well with docker-compose, so we wrap this logic in a screen session instead
 # you can use h2server with docker run, but the point of this setup is to run many servers via docker-compose
